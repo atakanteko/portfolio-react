35a7edcd6f74a87ac448b7b6dd2e6f98d2ffd2f0
atakantekoglu <atakantekoglu@gmail.com>
2022-11-15 00:22:48 +0300

    nav section

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import { BsFillMoonStarsFill } from "react-icons/all";
 
 export default function Home() {
   return (
@@ -9,10 +10,21 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-
+      <main className="bg-white px-10">
+        <section className="min-h-screen">
+            <nav className="py-10 mb-12 flex justify-between">
+                <h1 className="text-xl font-burtons">developedbyat</h1>
+                <ul className="flex items-center">
+                    <li>
+                        <BsFillMoonStarsFill className="cursor-pointer text-2xl"/>
+                    </li>
+                    <li>
+                        <a href="#" className="bg-cyan-500 text-white px-4 py-2 rounded-md ml-8">Resume</a>
+                    </li>
+                </ul>
+            </nav>
+        </section>
       </main>
-
     </div>
   )
 }
